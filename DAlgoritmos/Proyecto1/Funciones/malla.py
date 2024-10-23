24ea2a752677184fce6b1b8b4e602a5f7de53421
AltairAG <aaguilarg2024@cic.ipn.mx>
2024-10-23 13:50:37 -0600

    BFS, DFS_I, DFS_R Funcional(faltan algunos detalles).

--- DAlgoritmos/Proyecto1/Funciones/malla.py
+++ DAlgoritmos/Proyecto1/Funciones/malla.py
@@ -24,26 +24,12 @@ def grafoMalla(filas, columnas, dirigido=False):
                 if not dirigido:
                     grafo1.crear_aristas(grafo1.lista_nodos[vertice + columnas].id, grafo1.lista_nodos[vertice].id)  # Arista en ambas direcciones
 
-    nodo_fuente = 4  # El nodo fuente 
-    
-    print("Árbol BFS:")
-    arbol_bfs = grafo1.BFS(nodo_fuente)
-    for arista in arbol_bfs:
-        print(arista)
-
-    print("\nÁrbol DFS Recursivo:")
-    arbol_dfs_r = grafo1.DFS_R(nodo_fuente)
-    for arista in arbol_dfs_r:
-        print(arista)
-
-    print("\nÁrbol DFS Iterativo:")
-    arbol_dfs_i = grafo1.DFS_I(nodo_fuente)
-    for arista in arbol_dfs_i:
-        print(arista)
-    
+       
     
     grafo1.imprimir_lista_aristas()  # Imprimir aristas del grafo
 #    grafo1.guardar_csv("Malla\\Malla.csv")  # Guardar en CSV
     grafo1.guardar_graphviz("Malla\\Malla.gv")  # Guardar en formato GraphViz
 
     return grafo1
+
+
